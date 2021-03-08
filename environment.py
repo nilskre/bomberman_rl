fix ROWS and COLS for scaling down

environment.py
@@ -351,8 +351,8 @@ class BombeRLeWorld(GenericWorld):
                     if coin_pattern[i, j] == 1:
                         self.coins.append(Coin((x + i, x + j), self.arena[x+i,x+j] == 0))
                         coins[x + i, x + j] += 1"""
-        for i in range(3):
-            for j in range(3):
+        for i in range(int(s.GRID_SIZE / 10)):
+            for j in range(int(s.GRID_SIZE / 10)):
                 n_crates = (self.arena[1 + 5 * i:6 + 5 * i, 1 + 5 * j:6 + 5 * j] == 1).sum()
                 while True:
                     x, y = np.random.randint(1 + 5 * i, 6 + 5 * i), np.random.randint(1 + 5 * j, 6 + 5 * j)